647325e8a8eeabce2baa70a1fdd4704359fb4c1f
citex6 <gvhhhhui498@gmail.com>
2025-07-02 07:13:38 +0100

    Update loadstring.lua

--- prototype/loadstring.lua
+++ prototype/loadstring.lua
@@ -1,5 +1,35 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
 
+local screenGui = Instance.new("ScreenGui")
+screenGui.Name = "CenteredImageGui"
+screenGui.Parent = playerGui
+screenGui.DisplayOrder = 100  -- any number above 0; default is 0
 
+local img = Instance.new("ImageLabel")
+img.Parent = screenGui
+img.BackgroundTransparency = 1
+img.Image = "rbxassetid://85579381294022"
+
+img.Size = UDim2.new(0, 900, 0, 900)
+img.AnchorPoint = Vector2.new(0.5, 0.5)       -- Center the anchor
+img.Position = UDim2.new(0.5, 0, 0.5, 0)      -- Center position on screen
+img.BackgroundTransparency = 1
+
+local TweenService = game:GetService("TweenService")
+
+-- Wait 5 seconds, then fade out image
+task.delay(5, function()
+    local fadeOutTween = TweenService:Create(img, TweenInfo.new(1.5), {ImageTransparency = 1})
+    fadeOutTween:Play()
+    fadeOutTween.Completed:Connect(function()
+        img:Destroy() -- remove image after fade out
+    end)
+end)
+-- first the image appears for 5 seconds then fades out revealing the rest of the ui
+
+task.wait(2)
 
 -- avatar spoofer / escape menu spoofer / leaderStats spoofer
 task.defer(function()
@@ -20,4 +50,3 @@ end)
 task.defer(function()
 
 end)
-
